06466d0a9e50b0440d7e77f9ddac6a7c9369defd
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-09-25 20:28:43 +0530

    Values adding to local storage

---
+++ script.js
@@ -0,0 +1,21 @@
+add = document.getElementById("add");
+add.addEventListener("click", () =>{
+    console.log("Updating List...");
+    tit = document.getElementById('tit').value;
+    desc = document.getElementById('description').value;
+    date = document.getElementById('time').value;
+
+    if (localStorage.getItem('itemsJson')==null){
+        itemJsonArray = [];
+        itemJsonArray.push([tit, desc, date]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    }
+    else{
+        itemJsonArrayStr = localStorage.getItem('itemsJson')
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+        itemJsonArray.push([tit, desc, date]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    }
+
+    
+});
